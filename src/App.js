fc6e2210e0b8aeff6b61491774d42ed89fbf21e8
Charbel EID <charbeleid959@gmail.com>
2021-03-20 20:22:59 +0200

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <h1>hello</h1>;
+  return <Navbar />;
 }
 
 export default App;
